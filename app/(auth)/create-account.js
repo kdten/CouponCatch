e305a6c9cd3795173fe7a4b448d161722fb3c420
kdten <kolten.rasmussen@gmail.com>
2023-05-24 21:48:03 -0600

    firebase and local error handling

--- app/(auth)/create-account.js
+++ app/(auth)/create-account.js
@@ -1,4 +1,10 @@
-import { View, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback } from "react-native";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native";
 import { useRef, useState } from "react";
 import { AuthStore, appSignUp } from "../../store.js";
 import { Stack, useRouter } from "expo-router";
@@ -8,42 +14,42 @@ import PasswordStrengthMeterBar from "react-native-password-strength-meter-bar";
 // Define strength level list
 const strengthLevels = [
   {
-    label: 'Weak',
-    labelColor: '#fff',
-    widthPercent: '33',
-    innerBarColor: '#fe6c6c'
+    label: "Weak",
+    labelColor: "#fff",
+    widthPercent: "33",
+    innerBarColor: "#fe6c6c",
   },
   {
-    label: 'Weak',
-    labelColor: '#fff',
-    widthPercent: '33',
-    innerBarColor: '#fe6c6c'
+    label: "Weak",
+    labelColor: "#fff",
+    widthPercent: "33",
+    innerBarColor: "#fe6c6c",
   },
   {
-    label: 'Fair',
-    labelColor: '#fff',
-    widthPercent: '67',
-    innerBarColor: '#feb466'
+    label: "Fair",
+    labelColor: "#fff",
+    widthPercent: "67",
+    innerBarColor: "#feb466",
   },
   {
-    label: 'Fair',
-    labelColor: '#fff',
-    widthPercent: '67',
-    innerBarColor: '#feb466'
+    label: "Fair",
+    labelColor: "#fff",
+    widthPercent: "67",
+    innerBarColor: "#feb466",
   },
   {
-    label: 'Strong',
-    labelColor: '#fff',
-    widthPercent: '100',
-    innerBarColor: '#6cfeb5'
-  }
+    label: "Strong",
+    labelColor: "#fff",
+    widthPercent: "100",
+    innerBarColor: "#6cfeb5",
+  },
 ];
 
 // Define too short object
 const tooShort = {
   enabled: true,
-  label: 'Too short',
-  labelColor: 'red'
+  label: "Too short",
+  labelColor: "red",
 };
 
 export default function CreateAccount() {
@@ -55,40 +61,35 @@ export default function CreateAccount() {
   const [passwordError, setPasswordError] = useState(null);
   const [passwordIsValid, setPasswordIsValid] = useState(false);
 
-    // valdiate error messages, if errors !exist, then sends to database
-    const validate = () => {
-      if (!email.includes('@')){
-        setEmailError('Please enter a valid email address');
-      }
-  
-      else if (password.length < 8){
-        setPasswordError('Password must be at least 8 characters');
-      }
-  
-      else if (email.length === 0 )
-        setEmailError('Email is required');
-  
-      else if (email.indexOf(' ') >= 0) {
-        setEmailError('Email cannot contain spaces');
-      }
-  
-      else if (password.indexOf(' ') >= 0) {
-        setPasswordError('Password cannot contain spaces');
-      }
-
-      else if (password !== passwordRepeat) {
-        setPasswordError('Passwords do not match');
-      }
-  
-      else {
-        setEmailError('');
-        setPasswordError('');
-        return true;
-      }
+  // valdiate error messages, if errors !exist, then sends to database
+  const validate = () => {
+    if (!email.includes("@")) {
+      setEmailError("Please enter a valid email address");
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters");
+    } else if (email.length === 0) setEmailError("Email is required");
+    else if (email.indexOf(" ") >= 0) {
+      setEmailError("Email cannot contain spaces");
+    } else if (password.indexOf(" ") >= 0) {
+      setPasswordError("Password cannot contain spaces");
+    } else if (password !== passwordRepeat) {
+      setPasswordError("Passwords do not match");
+    } else {
+      setEmailError("");
+      setPasswordError("");
+      return true;
     }
+  };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Stack.Screen
         options={{ title: "Create Account", headerLeft: () => <></> }}
       />
@@ -99,11 +100,16 @@ export default function CreateAccount() {
           variant="outlined"
           nativeID="email"
           value={email}
-          onChangeText={(text) => { 
+          onChangeText={(text) => {
             setEmail(text);
           }}
         />
       </KeyboardAvoidingView>
+
+      <View style={{ width: 250, paddingHorizontal: 25, marginBottom: 10 }}>
+        <PasswordStrengthMeterBar password={password} />
+      </View>
+
       <KeyboardAvoidingView>
         <TextInput
           label="password"
@@ -111,23 +117,22 @@ export default function CreateAccount() {
           secureTextEntry={true}
           nativeID="password"
           value={password}
-          onChangeText={(text) => { 
+          onChangeText={(text) => {
             setPassword(text);
           }}
           style={styles.textInput}
         />
-        <PasswordStrengthMeterBar password={password} />
-
       </KeyboardAvoidingView>
+
       <KeyboardAvoidingView>
         <TextInput
-          size='short'
+          size="short"
           label="password repeat"
           variant="outlined"
           secureTextEntry={true}
           nativeID="passwordrepeat"
           value={passwordRepeat}
-          onChangeText={(text) => { 
+          onChangeText={(text) => {
             setPasswordRepeat(text);
           }}
           style={styles.textInput}
@@ -136,40 +141,56 @@ export default function CreateAccount() {
 
       <Text style={styles.validateErrorText}>{emailError}</Text>
       <Text style={styles.validateErrorText}>{passwordError}</Text>
-        
-      <View style={{ flexDirection: "column", justifyContent: "space-between", alignSelf: 'center', alignItems: 'center' }}>
-      <Button
-        onPress={async () => {
-          if (!validate()) return;
-          const resp = await appSignUp(
-            email,
-            password,
-          );
-          if (resp?.user) {
-            router.replace("/(tabs)/home");
-          } else {
-            console.log(resp.error);
-            Alert.alert("Sign Up Error", resp.error?.message);
-          }
+
+      <View
+        style={{
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignSelf: "center",
+          alignItems: "center",
         }}
-        title="Save your new account"
-        color="#000"
       >
-      </Button>
-        <br />
-      <Button
-        onPress={() => {
-          AuthStore.update((s) => {
-            s.isLoggedIn = false;
-          });
-          router.replace("/login");
-        }}
-        title="Cancel"
-        color="#000"
-      >
-      </Button>
+        <Button
+          onPress={async () => {
+            try {
+              if (!validate()) return;
+              const resp = await appSignUp(email, password);
+              if (resp?.user) {
+                router.replace("/(tabs)/home");
+              } else {
+                console.log(resp.error);
+                Alert.alert("Sign Up Error", resp.error?.message);
+              }
+            } catch (error) {
+              // Handle different error codes from Firebase
+              if (error.code === "auth/email-already-in-use") {
+                setEmailError("The email address is already in use.");
+              } else {
+                // Generic error message for other error codes
+                Alert.alert(
+                  "Sign Up Error",
+                  "An error occurred during sign up. Please try again."
+                );
+              }
+              console.log(error);
+            }
+          }}
+          title="Save your new account"
+          color="#000"
+        />
+        <View style={{ marginTop: 20 }} />
+        <Button
+          onPress={() => {
+            AuthStore.update((s) => {
+              s.isLoggedIn = false;
+            });
+            router.replace("/login");
+          }}
+          title="Cancel"
+          color="#000"
+        ></Button>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -184,8 +205,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   validateErrorText: {
-    color: 'red',
+    color: "red",
     fontSize: 15,
     marginBottom: 8,
-  }
+  },
 });
--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -3,7 +3,9 @@ import {
   StyleSheet,
   Alert,
   Keyboard,
+  KeyboardAvoidingView,
   TouchableWithoutFeedback,
+  ScrollView,
 } from "react-native";
 import { AuthStore, appSignIn } from "../../store.js";
 import { Stack, useRouter } from "expo-router";
@@ -14,10 +16,35 @@ export default function LogIn() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  // valdiate error messages, if errors !exist, then sends to database
+  const validate = () => {
+    if (!email.includes("@")) {
+      setEmailError("Please enter a valid email address");
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters");
+    } else if (email.length === 0) setEmailError("Email is required");
+    else if (email.indexOf(" ") >= 0) {
+      setEmailError("Email cannot contain spaces");
+    } else if (password.indexOf(" ") >= 0) {
+      setPasswordError("Password cannot contain spaces");
+    } else {
+      setEmailError("");
+      setPasswordError("");
+      return true;
+    }
+  };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View>
+    <ScrollView
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <KeyboardAvoidingView>
         <TextInput
           style={styles.textInput}
           label="email"
@@ -28,8 +55,8 @@ export default function LogIn() {
             setEmail(text);
           }}
         />
-      </View>
-      <View>
+      </KeyboardAvoidingView>
+      <KeyboardAvoidingView>
         <TextInput
           style={styles.textInput}
           label="password"
@@ -43,44 +70,72 @@ export default function LogIn() {
           }}
           color="primary"
         />
-      </View>
+      </KeyboardAvoidingView>
 
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View style={{ marginTop: 20 }} />
+      <Text style={styles.validateErrorText}>{emailError}</Text>
+      <Text style={styles.validateErrorText}>{passwordError}</Text>
+
+      <View
+        style={{
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <Button
           onPress={async () => {
-            const resp = await appSignIn(email, password);
-            if (resp?.user) {
-              router.replace("/(tabs)/home");
-            } else {
-              console.log(resp.error);
-              Alert.alert("Login Error", resp.error?.message);
+            try {
+              const resp = await appSignIn(email, password);
+              if (resp?.user) {
+                router.replace("/(tabs)/home");
+              } else {
+                console.log(resp.error);
+                Alert.alert("Login Error", resp.error?.message);
+              }
+            } catch (error) {
+              // Handle different error codes from Firebase
+              if (error.code === "auth/invalid-email") {
+                setEmailError(
+                  "Invalid email password combo. Please check and try again."
+                );
+              } else if (error.code === "auth/user-not-found") {
+                setEmailError(
+                  "No user found with this email. Please check the email and try again."
+                );
+              } else if (error.code === "auth/invalid-password") {
+                setEmailError(
+                  "Invalid email password combo. Please check and try again."
+                );
+              } else {
+                // Generic error message for other error codes
+                setEmailError(
+                  "An error occurred during login. Please try again."
+                );
+              }
+              console.log(error);
             }
           }}
           title="Login"
           color="#000"
         />
-        
+
+        <View style={{ marginTop: 20 }} />
+
+        <Text style={{ fontSize: 12 }}>Don't have an account?</Text>
         <Button
-          style={{ marginLeft: 24 }}
           onPress={() => {
+            if (!validate()) return;
             AuthStore.update((s) => {
               s.isLoggedIn = true;
             });
             router.push("/create-account");
           }}
-          title="Create"
+          title="Create One"
           color="#000"
         />
       </View>
-      {/* <View style={{ flexDirection: "row" }}>
-        <Button
-          style={{ marginTop: 12, backgroundColor: "red" }}
-          // login with Google here 
-          title="Login with Google"
-          trailing={props => <Ionicon name="logo-google" {...props} />}
-        />
-      </View> */}
-    </View>
+    </ScrollView>
   );
 }
 
@@ -96,11 +151,10 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   validateErrorText: {
-    color: 'red',
+    color: "red",
     fontSize: 15,
     marginBottom: 8,
-
-  }
+  },
 });
 
 const DismissKeyboard = ({ children }) => (
